c6215a5fd9b7fede7906d7159585a1e3ee773148
Souha Ghabri <107365513+SOUHA-GH@users.noreply.github.com>
2023-03-25 10:25:19 +0100

    Create camera_test.py

---
+++ Object_detection/camera_test.py
@@ -0,0 +1,35 @@
+#The purpose is to display the image captured from the camera. Used as a test program to verify if OpenCV has been properly installed.
+# ------------------------------------------------------------------------------
+
+import cv2
+
+CAMERA_DEVICE_ID = 0
+IMAGE_WIDTH = 320
+IMAGE_HEIGHT = 240
+
+if __name__ == "__main__":
+    try:
+        # create video capture
+        cap = cv2.VideoCapture(CAMERA_DEVICE_ID)
+
+        # set resolution to 320x240 to reduce latency 
+        cap.set(3, IMAGE_WIDTH)
+        cap.set(4, IMAGE_HEIGHT)
+
+        # Loop to continuously get images
+        while True:
+            # Read the frames from a camera
+            _, frame = cap.read()
+
+            # show image
+            cv2.imshow('frame', frame)
+
+            # if key pressed is 'Esc' then exit the loop
+            if cv2.waitKey(33) == 27:
+                break
+    except Exception as e:
+        print(e)
+    finally:
+        # Clean up and exit the program
+        cv2.destroyAllWindows()
+        cap.release()
